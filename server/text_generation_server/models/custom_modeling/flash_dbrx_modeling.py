00de2b1a29027f094282669c978a829b652f45a5
Nicolas Patry <patry.nicolas@protonmail.com>
2024-05-29 10:52:09 +0000

    HHachweew Hack to make other models work.

--- server/text_generation_server/models/custom_modeling/flash_dbrx_modeling.py
+++ server/text_generation_server/models/custom_modeling/flash_dbrx_modeling.py
@@ -444,6 +444,7 @@ class DbrxAttention(torch.nn.Module):
                 self.kv_head_mapping,
                 self.softmax_scale,
                 block_tables,
+                None,
                 input_lengths,
                 max_s,
             )
--- server/text_generation_server/models/custom_modeling/flash_mistral_modeling.py
+++ server/text_generation_server/models/custom_modeling/flash_mistral_modeling.py
@@ -249,6 +249,7 @@ class MistralAttention(torch.nn.Module):
                 self.kv_head_mapping,
                 self.softmax_scale,
                 block_tables,
+                None,
                 input_lengths,
                 max_s,
             )
